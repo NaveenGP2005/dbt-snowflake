60ffe0d875efb725e69a5b8c256126312e13b6c8
NaveenGP2005 <naveengpatil26@gmail.com>
2026-05-30 20:23:00 +0530

    fix

--- aws_dbt_snowflake/models/bronze/bronze_bookings.sql
+++ aws_dbt_snowflake/models/bronze/bronze_bookings.sql
@@ -1,1 +1,13 @@
-select * from {{source('staging','bookings')}}
+{{ config(
+    materialized='incremental'
+) }}
+
+select *
+from {{ source('staging','bookings') }}
+
+{% if is_incremental() %}
+where CREATED_AT > (
+    select coalesce(max(CREATED_AT), '1900-01-01')
+    from {{ this }}
+)
+{% endif %}
